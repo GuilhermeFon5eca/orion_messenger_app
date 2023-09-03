20fe3d81217a5533b17333f18179fcab25af1aab
Guilherme Fonseca <44032248+GuilhermeFon5eca@users.noreply.github.com>
2023-09-03 14:00:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# orion_messenger_app
+ Test for Pusher on Laravel
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Events\WebsocketDemoEvent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    broadcast(new App\Events\WebsocketDemoEvent('some data'));
+    return view('welcome');
+});
+
+
+Route::get('/main', [App\Http\Controllers\MainController::class, 'index']);
+Route::get('/chats', [App\Http\Controllers\ChatsController::class, 'index']);
+
+Route::get('/messages', [App\Http\Controllers\ChatsController::class, 'fetchMessages']);
+Route::post('/messages', [App\Http\Controllers\ChatsController::class, 'sendMessage']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
